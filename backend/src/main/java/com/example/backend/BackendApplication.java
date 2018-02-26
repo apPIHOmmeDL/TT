fe39fc1406e3b37b8d5678a9d135e8986cf3c735
AliideV <aliide.vainikko@gmail.com>
2018-02-26 14:28:26 +0200

    test2

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -1,5 +1,6 @@
 package com.example.backend;
 //Siin on kommentaar
+//siin on teine kommentaar ka
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
