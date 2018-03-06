473e1cbc0497ec5845659aeaac1f4836dea0d9dc
vaheks <vaheks@gmail.com>
2018-03-06 18:18:54 +0200

    Add files via upload
    professori kontroller

---
+++ backend/src/main/java/com/example/backend/prof/ProfController.java
@@ -0,0 +1,35 @@
+package com.example..backend.prof;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProfController {
+	
+	private ProfService profService;
+
+	public ProfController(ProfService profService) {
+		this.profService = profService;
+	}
+	
+	@RequestMapping(value="/profs/add", method=RequestMethod.POST,
+			consumes = "application/json")
+	public Prof addProf(@RequestBody Prof prof) {
+		return profService.addProf(prof);
+	}
+	
+	@RequestMapping(value="/profs", method=RequestMethod.GET)
+	public List<Prof> getAllProfs() {
+		return profService.getAllProfs();
+	}
+	
+	@RequestMapping(value = "/profs/{id}", method=RequestMethod.GET)
+	public Prof getProf(@PathVariable("id") long profId) {
+		return profService.getProfById(profId);
+	}
+}
