6139a544e8c500a1f3df2f30a3804db5abe3d437
annenna <asiitan@gmail.com>
2018-03-09 19:13:17 +0200

    anne- töötav frontend

--- frontend/src/prof/prof.js
+++ frontend/src/prof/prof.js
@@ -1,7 +1,35 @@
+import {HttpClient, json} from 'aurelia-fetch-client'
+
+
 export class Professor {
+
+
     constructor(firstname, lastname, subject) {
         this.firstname = firstname;
         this.lastname = lastname;
         this.subject = subject;
+    }
+    attached(){
+        console.log ("ATTACHED")
+    }
+    addUser(){
+
+        let client = new HttpClient();
+
+        this.userData = {
+            "firstName":"Mari",
+            "lastName":"Roo",
+            "subject":"Mata",
+        };
+
+            client.fetch('http://localhost:8080/profs/add', {
+            'method':"POST",
+            'body': json(this.userData),
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log("Server saatis " + data.firstName + data.lastName+ data.subject);
+    });
+        console.log("Method executed!")
     }
 }
