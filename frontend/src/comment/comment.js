1add14bb7e6ca4e66a29a55a43470aea08c10872
AliideV <aliide.vainikko@gmail.com>
2018-04-10 09:55:46 +0300

    Kommentaaride lehele suunamine - nüüd otse Array id-ga + kommentaaride leht ilusaks!

--- frontend/src/comment/comment.js
+++ frontend/src/comment/comment.js
@@ -1,31 +1,20 @@
 import {HttpClient, json} from 'aurelia-fetch-client'
 import environment from "../environment";
 
-
 export class comment{
 
     activate(params, routeConfig){
         let client = new HttpClient();
-        console.log(params);
-        console.log(params.pid);
-        console.log(params.sid);
-       // console.log(pid.teachings[sid].subject.title);
+        this.ssid = params.sid;
+        console.log(this.ssid);
         client.fetch(environment.URL + 'profs/' + params.pid)
             .then(response => response.json())
             .then(singleProf => this.singleProf = singleProf);
-
-        client.fetch(environment.URL + 'subjects/' + params.sid)
-            .then(response => response.json())
-            .then(singleSubj => this.singleSubj = singleSubj);
-
     }
+
 
     constructor(router){
         this.router = router;
-    }
-
-    get profFullName(){
-        return '${this.firstName} ${this.lastName}';//return '${this.firstName} ${this.lastName}';
     }
 
     createComment(){
